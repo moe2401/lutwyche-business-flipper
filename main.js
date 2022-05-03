af39b2349987d5902cefde7440e91237396dc507
barry jb <barryjb4@gmail.com>
2022-05-03 19:08:40 +1000

    fixed javascript functionality for automatic images

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 let i = 0;
 let images = [];
-let time = 1000;
+let time = 3000;
 
 //image list
 images[0] = "img/artisan.jpeg";
 images[1] = "img/auzus.jpeg";
 images[2] = "img/barber-shop.jpeg";
 images[3] = "img/bestnails.jpeg";
+images[4] = "img/epic-hair.jpeg";
+images[5] = "img/hanesushi.png";
+images[6] = "img/hot-bargain.jpeg";
+images[7] = "img/iSmart.jpeg";
+images[8] = "img/kaisercraft.jpeg";
+images[9] = "img/laraghy-and-chan.jpeg";
+images[10] = "img/massage-indulgence.jpeg";
+images[11] = "img/skin-centre.jpeg";
 
 //change image
 function changeImg(){
-    document.slide.src = images[i];
-    // document.getElementById("main-img").style.backgroundImage = images[i];
+    document.getElementById("main-img").style.backgroundImage = "url(" + images[i] +")";
 
     if(i < images.length - 1){
         i++;
@@ -22,22 +29,4 @@ function changeImg(){
     setTimeout("changeImg()", time);
 }
 
-
-
-//create array of images
-// let images = ["img/artisan.jpeg", "img/.jpeg", "img/barber-shop.jpeg"];
-
-//automatic changing image
-// function change(){
-//     i++;
-//     if(i >= images.length){
-//         i = 0;
-//     }
-// }
-// document.getElementById("main-img").style.backgroundImage = "url(img + [i])" 
-
-//when you click button, image changes
-// function clickMe(){
-//     document.getElementById("main-img").style.backgroundImage = "url(img/auzus.jpeg)" 
-// };
-
+window.onload = changeImg();
