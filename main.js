4f03a83190aca8de013a8eb835cc3917e8f212bc
moe <moe.aoki0124@gmail.com>
2022-05-03 19:12:24 +1000

    updated html and todo.txt

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let i = 0;
 let images = [];
-let time = 1000;
+let time = 3000;
 
 //image list
 images[0] = "img/artisan.jpeg";
@@ -10,7 +10,7 @@ images[3] = "img/bestnails.jpeg";
 
 //change image
 function changeImg(){
-    document.slide.src = images[i];
+    document.getElementById("main-img").style.backgroundImage = "url(" + images[i] + ")";
     // document.getElementById("main-img").style.backgroundImage = images[i];
 
     if(i < images.length - 1){
@@ -22,22 +22,4 @@ function changeImg(){
     setTimeout("changeImg()", time);
 }
 
-
-
-//create array of images
-// let images = ["img/artisan.jpeg", "img/.jpeg", "img/barber-shop.jpeg"];
-
-//automatic changing image
-// function change(){
-//     i++;
-//     if(i >= images.length){
-//         i = 0;
-//     }
-// }
-// document.getElementById("main-img").style.backgroundImage = "url(img + [i])" 
-
-//when you click button, image changes
-// function clickMe(){
-//     document.getElementById("main-img").style.backgroundImage = "url(img/auzus.jpeg)" 
-// };
-
+window.onlaod = changeImg();
